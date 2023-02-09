37c9da8322799df2007e8a8ac713be38358139f9
Abhi1Man <118902846+Abhi1Man@users.noreply.github.com>
2023-02-09 13:39:15 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+import Note from "./Note";
+import Footer from "./Footer";
+ReactDOM.render(
+<div>
+  <Header></Header>
+  <Note></Note>
+  <Footer></Footer>
+  </div>
+  ,document.getElementById('root'));
